4ead17e8afe4d8dd25a53565f7e6ae2e5e7fe5d6
OosOkami <96697153+OosOkami@users.noreply.github.com>
2022-01-01 09:51:44 +0100

    fond de chaque écran

--- Outils/Scriptoutils.js
+++ Outils/Scriptoutils.js
@@ -1,29 +1,61 @@
-const commerce = document.querySelector(".Commerce");
+// const commerce = document.querySelector(".Commerce");
 
-console.log(commerce);
+// console.log(commerce);
 
-commerce.addEventListener("click", (e) => {
-    commerce.classList.add("popup");
-    commerce.classList.remove("giftDim");
+// commerce.addEventListener("click", (e) => {
+//     commerce.classList.add("popup");
+//     commerce.classList.remove("giftDim");
 
-    const newiframe =document.createElement("iframe");
-    newiframe.classList.add("sitepopup");
-    newiframe.setAttribute("src","https://sc-trade.tools/home");
-    commerce.appendChild(newiframe);
+//     const newiframe =document.createElement("iframe");
+//     newiframe.classList.add("sitepopup");
+//     newiframe.setAttribute("src","https://sc-trade.tools/home");
+//     commerce.appendChild(newiframe);
 
     
+// });
+
+// const minage = document.querySelector(".minage")
+
+// console.log(minage);
+
+// minage.addEventListener("click", (e) => {
+//     minage.classList.add("popup");
+//     minage.classList.remove("giftDim");
+    
+//     const newiframe =document.createElement("iframe");
+//     newiframe.classList.add("sitepopup");
+//     newiframe.setAttribute("src","https://uexcorp.space/");
+//     minage.appendChild(newiframe);
+// })
+
+// const discord = document.querySelector(".discord")
+
+// console.log(discord);
+
+// discord.addEventListener("click", (e) => {
+//     discord.classList.add("popup");
+//     discord.classList.remove("giftDim");
+    
+//     const newiframe =document.createElement("iframe");
+//     newiframe.classList.add("sitepopup");
+//     newiframe.setAttribute("src","https://discord.gg/FU8sdF9B");
+//     minage.appendChild(newiframe);
+// });
+
+
+
+///////////////////  Déploiement de section ///////////////////
+
+const sections = document.querySelectorAll(".section");
+
+console.log(sections);
+sections.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        console.log(e.target);
+        e.target.style.width = 90 + "%";
+        e.target.style.marginLeft = 10 + "%";
+
+        
+
+    });
 });
-
-const minage = document.querySelector(".minage")
-
-console.log(minage);
-
-minage.addEventListener("click", (e) => {
-    minage.classList.add("popup");
-    minage.classList.remove("giftDim");
-    
-    const newiframe =document.createElement("iframe");
-    newiframe.classList.add("sitepopup");
-    newiframe.setAttribute("src","https://uexcorp.space/");
-    minage.appendChild(newiframe);
-})
